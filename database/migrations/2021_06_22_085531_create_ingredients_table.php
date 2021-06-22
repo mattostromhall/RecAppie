0b9162e07e5560f8a2316ac71c9b43eaa07c0189
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2021-06-22 11:03:08 +0100

    added ingredient model and migration

---
+++ database/migrations/2021_06_22_085531_create_ingredients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ingredients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('recipe_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('name');
+            $table->unsignedInteger('quantity');
+            $table->string('unit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ingredients');
+    }
+}
