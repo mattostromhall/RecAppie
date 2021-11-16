23413cf605a305cd893585f024cb97ce9f4e046b
Matt Ostrom-Hall <matthew.ostrom.hall@gmail.com>
2021-11-16 16:17:12 +0000

    dashboard and nav updates

--- resources/js/Components/MainNav.js
+++ resources/js/Components/MainNav.js
@@ -22,7 +22,7 @@ export default function MainNav() {
                             </div>
                             <div className="hidden md:block">
                                 <div className="ml-10 flex items-baseline space-x-4">
-                                    <InertiaLink href="/dashboard" className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</InertiaLink>
+                                    <InertiaLink href="/dashboard" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Dashboard</InertiaLink>
                                     <InertiaLink href="/recipes" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Recipes</InertiaLink>
                                     <InertiaLink href="/recipes/create" className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Add Recipe</InertiaLink>
                                 </div>
